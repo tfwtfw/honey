a279e5cef9f3393b18c475e2ab9f3ed96ef4b705
toma <tomohito.fujita.world@gmail.com>
2023-02-28 00:53:18 +0900

    ♻️ .envファイル適用

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://ehnvkfofcztyqdbhfzlk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVobnZrZm9mY3p0eXFkYmhmemxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc0ODQ2NTgsImV4cCI6MTk5MzA2MDY1OH0.5DgcJtyBTk2MPJHwyZdz-w0y7XKobcARa7JPc7Jw63U')
+
+export const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
 
